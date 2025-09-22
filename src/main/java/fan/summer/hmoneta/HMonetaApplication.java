dc0fd35b3a057615b131728f2dfda1d5a546f472
MuskStart <gitmain@outlook.sg>
2025-09-22 10:32:35 +0800

    :sparkles: 增加支持数据库实体扫描

--- src/main/java/fan/summer/hmoneta/HMonetaApplication.java
+++ src/main/java/fan/summer/hmoneta/HMonetaApplication.java
@@ -2,8 +2,10 @@ package fan.summer.hmoneta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("fan.summer.hmoneta.database.entity")
 public class HMonetaApplication {
 
     void main(String[] args) {
