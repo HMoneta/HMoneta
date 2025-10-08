be10e2aed037af17a0d132fc4e476e906cdf20ae
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-10-08 23:08:17 +0800

    :sparkles: 增加拦截配置

---
+++ src/main/java/fan/summer/hmoneta/common/config/HMInterceptor.java
@@ -0,0 +1,25 @@
+package fan.summer.hmoneta.common.config;
+
+
+import fan.summer.hmoneta.common.interceptor.ApiInterceptor;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Component
+public class HMInterceptor implements WebMvcConfigurer {
+
+
+    @Resource
+    private ApiInterceptor apiInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/hm/user/login");
+    }
+}
