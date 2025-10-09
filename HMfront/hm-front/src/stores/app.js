ef77cac373302512a847f6350d7bbcc09b2da0c7
MuskStart <gitmain@outlook.sg>
2025-10-09 18:08:46 +0800

    :bug: 修复网页退出后缓存未能清理的Bug以及修复使用非法token绕过登陆界面的Bug

--- HMfront/hm-front/src/stores/app.js
+++ HMfront/hm-front/src/stores/app.js
@@ -1,26 +1,37 @@
 // Utilities
 import { defineStore } from 'pinia'
+import {http} from "@/common/request.js"
 
 export const useUserStore = defineStore('user', {
   state: () => ({
     token: null,
+    isValidToken: false,
   }),
   actions: {
     login(token) {
       this.token = token;
-      localStorage.setItem('HMToken', token);
+      sessionStorage.setItem('HMToken', token);
     },
     logout() {
       this.token = null;
-      localStorage.removeItem('HMToken');
+      sessionStorage.removeItem('HMToken');
     },
-    checkAuth() {
-      const token = localStorage.getItem('HMToken');
+    async checkAuth() {
+      const token = sessionStorage.getItem('HMToken');
       if (token) {
-        this.token = token;
-        return true;
-      }else {
-        localStorage.removeItem('HMToken');
+        const valid = await http.get('/user/valid', {HMToken: token})
+        if (valid) {
+          this.token = token;
+          this.isValidToken = valid;
+          return valid;
+        }else {
+          sessionStorage.removeItem('HMToken');
+          this.isValidToken = false;
+          return false;
+        }
+      } else {
+        sessionStorage.removeItem('HMToken');
+        this.isValidToken = false;
         return false;
       }
     },
