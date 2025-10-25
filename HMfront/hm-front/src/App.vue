741d63df35d5fe69a86d7766ac4adc0d4068f975
MuskStart <138420547+MuskStark@users.noreply.github.com>
2025-10-25 23:16:38 +0800

    :sparkles: 增加全局通知Pinpa

--- HMfront/hm-front/src/App.vue
+++ HMfront/hm-front/src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <v-app>
-    <router-view />
+    <router-view/>
+    <!-- 全局通知 -->
+    <v-snackbar
+      v-model="notificationStore.show"
+      :color="notificationStore.color"
+      :timeout="3000"
+      location="bottom"
+    >
+      {{ notificationStore.message }}
+      <template v-slot:actions>
+        <v-btn
+          variant="text"
+          @click="notificationStore.show = false"
+        >
+          关闭
+        </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
+
 </template>
 
 <script setup>
-  //
+import {userNotificationStore} from '@/stores/app.js';
+
+const notificationStore = userNotificationStore();
 </script>
