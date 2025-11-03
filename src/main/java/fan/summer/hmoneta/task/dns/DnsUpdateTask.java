88d68e2380fe34866d4c8d1a2206c8ff8187b2ee
MuskStart <gitmain@outlook.sg>
2025-11-03 19:07:14 +0800

    :loud_sound: 增加log

--- src/main/java/fan/summer/hmoneta/task/dns/DnsUpdateTask.java
+++ src/main/java/fan/summer/hmoneta/task/dns/DnsUpdateTask.java
@@ -3,8 +3,7 @@ package fan.summer.hmoneta.task.dns;
 import fan.summer.hmoneta.database.repository.dns.DnsResolveUrlRepository;
 import fan.summer.hmoneta.service.dns.DnsService;
 import fan.summer.hmoneta.util.IpUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -16,9 +15,9 @@ import org.springframework.stereotype.Component;
  * @Date 2025/11/1
  */
 @Component
+@Log4j2
 public class DnsUpdateTask {
 
-    private static final Logger logger = LogManager.getLogger(DnsUpdateTask.class);
     private final DnsService dnsService;
     private final DnsResolveUrlRepository dnsResolveUrlRepository;
 
@@ -31,18 +30,29 @@ public class DnsUpdateTask {
         this.latestIp = null;
     }
 
-    @Scheduled(fixedRate = 60000)
+    @Scheduled(fixedRate = 600000)
     public void updater() {
+        log.info("===============开始DDNS定时任务===============");
+        log.info("-开始查询公网IP");
         String publicIp = IpUtil.getPublicIp();
+        log.info("-开始比对公网IP是否变化");
         try {
             if (!publicIp.equals(latestIp)) {
+                log.info("-公网IP发生变更，开始更新DNS解析");
                 dnsResolveUrlRepository.findAll().forEach(dnsResolveUrl -> {
+                    log.info("开始为{}更新DNS解析", dnsResolveUrl.getUrl());
                     dnsService.updateDnsResolveUrl(dnsResolveUrl, publicIp);
+                    log.info("DNS解析更新完成");
                 });
+
+            }else {
+                log.info("-公网IP未发生变更，无需更新DNS解析");
             }
             this.latestIp = publicIp;
         } catch (Exception e) {
-            logger.error(e.getMessage());
+            log.error(e.getMessage());
+        }finally {
+            log.info("===============结束DDNS定时任务===============");
         }
     }
 }
