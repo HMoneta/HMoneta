2679017c38242dc6d97e4761dbc6c567dd356e7e
MuskStart <138420547+MuskStark@users.noreply.github.com>
2026-01-17 00:14:37 +0800

    :sparkles: 创建unifi配置前端界面

--- HMfront/hm-front/src/pages/setting.vue
+++ HMfront/hm-front/src/pages/setting.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {ref} from 'vue'
 import {http} from "@/common/request.js";
+import {userNotificationStore} from "@/stores/app.js";
 
 const tab = ref(null)
 // acme设置
@@ -11,12 +12,27 @@ const acmeInfo = reactive({
 const onSubmit = async () => {
   await http.post("/acme/modify", acmeInfo);
 }
+
+const unifiInfo = reactive({
+  baseUri: null,
+  apiKey: null,
+})
+
+const onUnifiSubmit = async () => {
+  try {
+    await http.post("/unifi/setting", {"baseUri": unifiInfo.baseUri, "apiKey": unifiInfo.apiKey});
+    userNotificationStore().showSuccess("设置保存成功")
+  } catch (err) {
+
+  }
+}
 </script>
 
 <template>
   <v-sheet elevation="4">
     <v-tabs v-model="tab" color="cyan" bg-color="lime-lighten-2">
       <v-tab value="network">网络配置</v-tab>
+      <v-tab value="api">外部Api</v-tab>
     </v-tabs>
 
     <v-divider></v-divider>
@@ -33,6 +49,17 @@ const onSubmit = async () => {
                 <v-btn color="primary" @click="onSubmit" variant="flat">提交</v-btn>
               </v-col>
             </v-row>
+          </v-card>
+        </v-sheet>
+      </v-tabs-window-item>
+      <v-tabs-window-item value="api">
+        <v-sheet class="pa-5">
+          <v-card class="pa-5" title="UnifiApi设置">
+            <v-text-field clearable label="UnifiApiBaseUrl" variant="solo-filled"
+                          v-model="unifiInfo.baseUri" class="mt-3"></v-text-field>
+            <v-text-field clearable label="UnifiApiKey" variant="solo-filled"
+                          v-model="unifiInfo.apiKey" class="mt-3"></v-text-field>
+            <v-btn color="primary" @click="onUnifiSubmit" variant="flat">提交</v-btn>
           </v-card>
         </v-sheet>
       </v-tabs-window-item>
