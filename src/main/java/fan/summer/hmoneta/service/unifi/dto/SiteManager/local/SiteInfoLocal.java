5acdde5ef8dd1a433161d56aebffb4f0ef22a668
MuskStart <138420547+MuskStark@users.noreply.github.com>
2026-01-19 22:56:45 +0800

    :sparkles: Unifi-site接口响应类

---
+++ src/main/java/fan/summer/hmoneta/service/unifi/dto/SiteManager/local/SiteInfoLocal.java
@@ -0,0 +1,17 @@
+package fan.summer.hmoneta.service.unifi.dto.SiteManager.local;
+
+import lombok.Data;
+
+/**
+ * UniFi Site Manager 本地数据项
+ *
+ * @author phoebej
+ * @version 1.00
+ * @Date 2026/1/18
+ */
+@Data
+public class SiteInfoLocal {
+    private String id;
+    private String internalReference;
+    private String name;
+}
