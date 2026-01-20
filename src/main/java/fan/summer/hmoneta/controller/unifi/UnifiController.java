2e34a7f641fd66e454ee3cf664d93affe96f8f28
MuskStart <138420547+MuskStark@users.noreply.github.com>
2026-01-20 23:49:55 +0800

    :construction: 构建UnifiController

--- src/main/java/fan/summer/hmoneta/controller/unifi/UnifiController.java
+++ src/main/java/fan/summer/hmoneta/controller/unifi/UnifiController.java
@@ -3,6 +3,9 @@ package fan.summer.hmoneta.controller.unifi;
 import fan.summer.hmoneta.controller.unifi.dto.UnifiSettingDto;
 import fan.summer.hmoneta.database.entity.unifi.UnifiSettingEntity;
 import fan.summer.hmoneta.service.unifi.UnifiApiService;
+import fan.summer.hmoneta.service.unifi.dto.SiteManager.local.SiteInfoLocal;
+import fan.summer.hmoneta.service.unifi.dto.SiteManager.local.SiteManagerLocalResp;
+import fan.summer.hmoneta.service.unifi.dto.SiteManager.local.client.ClientsLocalInfo;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -37,5 +40,20 @@ public class UnifiController {
     public ResponseEntity<Map<String, String>> getUnifiSetting() {
         UnifiSettingEntity unifiSettingEntity = unifiApiService.queryUnifiSettingInfo();
         return ResponseEntity.ok(Map.of("baseUri", unifiSettingEntity.getBaseUri()));
+    }
+
+    // sites
+    @GetMapping("/sites/info")
+    public ResponseEntity<SiteManagerLocalResp<SiteInfoLocal>> getAllSitesInfo() {
+        SiteManagerLocalResp<SiteInfoLocal> resp = unifiApiService.listLocalSites();
+        return ResponseEntity.ok(resp);
+    }
+
+    // Clients
+    @GetMapping("/sites/clients/info")
+    public ResponseEntity<SiteManagerLocalResp<ClientsLocalInfo>> getAllConnectedClients(@RequestParam String siteId) {
+        SiteManagerLocalResp<ClientsLocalInfo> resp = unifiApiService.listConnectedClients(siteId);
+        return ResponseEntity.ok(resp);
+
     }
 }
