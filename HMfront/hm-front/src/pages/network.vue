a466f3e5d2926bdd2fa6b602e1862ee9f57402bb
MuskStart <138420547+MuskStark@users.noreply.github.com>
2026-01-25 21:48:35 +0800

    :construction: 增加检查是否可以访问UnifiApi

--- HMfront/hm-front/src/pages/network.vue
+++ HMfront/hm-front/src/pages/network.vue
@@ -1,6 +1,12 @@
 <script setup>
 import {http} from "@/common/request.js";
 
+// Unifi相关代码
+const toolbarTitle = ref();
+const unifiStatus = ref(false);
+const checkUnifiAccessible = async () => {
+  return await http.get("/unifi/status")
+}
 const siteLists = ref();
 const siteClientsMap = ref({})
 const siteConnectedClientsInfo = ref();
@@ -13,7 +19,6 @@ const querySites = async () => {
 
   const clientsMap = {}
   for (const site of resp.data) {
-    const a = await queryClientsBySiteId(site.id)
     clientsMap[site.id] = await queryClientsBySiteId(site.id)
   }
 
@@ -33,17 +38,25 @@ const clientsHeaders = ref([
   {title: 'Ip地址', key: 'ipAddress', align: 'center'},
 ])
 onMounted(() => {
-  querySites()
+  unifiStatus.value = checkUnifiAccessible()
+  if (unifiStatus.value) {
+    toolbarTitle.value = '已连接Unifi'
+    querySites()
+  }
+
 })
 </script>
 
 <template>
+
   <v-card class="mt-6" v-for="site in siteLists" :key="site.id">
-    <v-data-table-server
+    <v-toolbar density="compact" :elevation="8" :title="toolbarTitle"></v-toolbar>
+    <v-data-table
       :headers="clientsHeaders"
       :items="siteClientsMap[site.id] || []"
       item-value="name"
-    ></v-data-table-server>
+      :items-per-page="10"
+    ></v-data-table>
 
   </v-card>
 </template>
